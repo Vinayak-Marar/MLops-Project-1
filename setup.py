2323a8a5aca5f85461e4ba8ae9ca8992fdaee40e
unknown <vinayakjeethmarar@gmail.com>
2025-07-19 15:02:59 +0530

    template initialized

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='us_visa',
+    version="0.0.1",
+    author='Vinayak Marar',
+    author_email='vinayakjeethmarar@gmail.com',
+    packages=find_packages()
+)
