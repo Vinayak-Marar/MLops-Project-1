60e913d7c16a039093a9a7b7fa902039aee33cb5
unknown <vinayakjeethmarar@gmail.com>
2025-07-20 11:05:26 +0530

    created exception file

--- demo.py
+++ demo.py
@@ -2,3 +2,16 @@
 
 # logging.info("This is to test logging file")
 #-------------------------------------------------------------------------
+# from us_visa.logger import logging
+# from us_visa.exception import USvisaException
+# import sys
+
+
+# try:
+#     a = 1 + 'Z'
+# except Exception as e:
+#     logging.info(e)
+#     raise USvisaException(e, sys) from e
+
+#----------------------------------------------------------------------------------------
+
