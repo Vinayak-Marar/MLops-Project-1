65c4da284ce2f3114b9f4e7340fb418fa1905db9
unknown <vinayakjeethmarar@gmail.com>
2025-07-22 21:01:59 +0530

    data ingestion added 3

--- demo.py
+++ demo.py
@@ -15,11 +15,12 @@
 
 #----------------------------------------------------------------------------------------
 # test DataIngestiion
-from us_visa.entity.artifact_entity import DataIngestionArtifact
-from us_visa.entity.config_entity import DataIngestionConfig
-from us_visa.components.data_ingestion import DataIngestion
+# from us_visa.entity.artifact_entity import DataIngestionArtifact
+# from us_visa.entity.config_entity import DataIngestionConfig
+# from us_visa.components.data_ingestion import DataIngestion
 
-data_ingestion_config = DataIngestionConfig()
-data_ingestion = DataIngestion(data_ingestion_config)
-result = data_ingestion.initiate_data_ingestion()
-print(result)
+# data_ingestion_config = DataIngestionConfig()
+# data_ingestion = DataIngestion(data_ingestion_config)
+# result = data_ingestion.initiate_data_ingestion()
+
+# print(result)
