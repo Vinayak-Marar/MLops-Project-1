ebb5d774e06072d5921bf96cbc0cbcbd3de5ffd5
unknown <vinayakjeethmarar@gmail.com>
2025-07-22 20:36:58 +0530

    data_ingestion added

--- demo.py
+++ demo.py
@@ -14,3 +14,12 @@
 #     raise USvisaException(e, sys) from e
 
 #----------------------------------------------------------------------------------------
+# test DataIngestiion
+from us_visa.entity.artifact_entity import DataIngestionArtifact
+from us_visa.entity.config_entity import DataIngestionConfig
+from us_visa.components.data_ingestion import DataIngestion
+
+data_ingestion_config = DataIngestionConfig()
+data_ingestion = DataIngestion(data_ingestion_config)
+result = data_ingestion.initiate_data_ingestion()
+print(result)
